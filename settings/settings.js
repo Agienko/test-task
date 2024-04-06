78a703a1f5f8a44df03bc35099567004a6f644ca
Dev BW <devbw@proton.me>
2024-04-06 14:25:11 +0300

    update

---
+++ settings/settings.js
@@ -0,0 +1,28 @@
+
+export const settings = {
+        game:{
+
+        },
+        loader:{
+            textures:[
+                // {
+                //     "name": "bg_main_port",
+                //     "srcs": "res/textures/bg_main_port.json"
+                // },
+            ],
+            spines: [
+                // {
+                //     "name": "symbol_bonus",
+                //     "srcs": "res/spines/symbol_bonus.json"
+                // },
+            ],
+            fonts: [
+                // {
+                //     "name": "gas",
+                //     "srcs": "res/fonts/gas.woff",
+                // },
+
+            ],
+
+        }
+}
